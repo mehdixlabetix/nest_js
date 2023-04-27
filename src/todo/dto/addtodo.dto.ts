525c6c81a805ded7459fddd8adf8e16c21eb2f16
Mehdi Cherif <mehdi.cherif@insat.ucar.tn>
2023-04-27 22:30:41 +0100

    tp3

---
+++ src/todo/dto/addtodo.dto.ts
@@ -0,0 +1,16 @@
+import {IsNotEmpty, IsNumber, IsString, MaxLength, MinLength, ValidationArguments,} from "class-validator";
+import {LengthErrorMessage, RequiredFieldErrorMessage} from "../../common/errors";
+
+export class AddTodoDto {
+  
+  @IsString()
+  @IsNotEmpty(RequiredFieldErrorMessage({targetName: 'name'} as ValidationArguments))
+  @MinLength(3, LengthErrorMessage(true, 3))
+  @MaxLength(10,LengthErrorMessage(false, 10) )
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10,LengthErrorMessage(true, 10))
+  description: string;
+}
